91ab8f027bbb576444db88c92a40a08951ddd9bf
Fareez Iqmal <60868965+iqfareez@users.noreply.github.com>
2023-05-12 23:21:15 +0800

    :bug: FIx file casing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Card from "./components/Card";
-import Biodata from "./components/biodata";
+import Biodata from "./components/Biodata";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div className="flex-row">
